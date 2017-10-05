e0d603b4a44f8ed960f565a6c58a11c2ebe2a0c3
Matt Ingram <mingram@vt.edu>
2017-10-05 01:55:18 -0400

    MI - Finalized port configuration for hosting mvcapp in Docker container in Azure mapping port 8080 to port 80 in Azure.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM microsoft/aspnetcore
 WORKDIR /app
 COPY pub .
-EXPOSE 80
-EXPOSE 22
-ENTRYPOINT ["dotnet", "mvcapp.dll"]
+EXPOSE 8080
+ENTRYPOINT [ "dotnet", "mvcapp.dll" ]
--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace mvcapp
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:80")
+                .UseUrls("http://+:8080")
                 .UseStartup<Startup>()
                 .Build();
     }
